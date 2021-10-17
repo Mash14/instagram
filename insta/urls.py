64fd16f8fdd5ba71082ca42da84e4ad91901bc63
mash <mashalonzo741@gmail.com>
2021-10-17 14:01:45 +0300

    Add the index page

--- insta/urls.py
+++ insta/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-
+    url('^$',views.home,name = 'imageIndex'),
 ]
 
 if settings.DEBUG:
--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from .models import Image,Comment,Profile
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    photos = Image.objects.all()
+
+    title = 'Home' 
+    return render(request, 'gram/index.html',{"photos":photos,"title":title})
